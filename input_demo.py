84ba06108ed5e35e548d5872d755f7471ab47871
subaoqi <subaoqi@clipworks.com>
2021-08-03 13:12:57 +0800

    aaa

---
+++ input_demo.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# Author: Yezishu
+# Data: 2021/7/29 12:26
+
+message = input("请输入：")
+print(message)
--- string_demo.py
+++ string_demo.py
@@ -2,26 +2,27 @@
 作者：Coco
 日期：2021年07月26日
 """
-name = 'bOb'
+
+name = 'boB'
 sex = 'man'
 hobby = ' ball '
 # 字符串常用函数
-# 首字母大写
+# 首字母大写 title()
 print(name.title())
-# 全部大写
+# 全部大写 upper()
 print(name.upper())
-# 全部小写
+# 全部小写 lower()
 print(name.lower())
 # 字符串拼接
 person = f'{name}是{sex}'
 print(person)
-# 删除末尾空白
+# 删除末尾空白(只是展示删除，原来的变量没有变)
 print(hobby)
 hobby_rstrip = hobby.rstrip()
 print(hobby_rstrip)
-# 删除开头的空白
+# 删除开头的空白(只是展示删除，原来的变量没有变)
 hobby_lstrip = hobby.lstrip()
 print(hobby_lstrip)
-# 删除首尾的空白
+# 删除首尾的空白(只是展示删除，原来的变量没有变)
 hobby_strip = hobby.strip()
 print(hobby_strip)
