3f6262bc998a354da4166420036638c79d5886ba
subaoqi <subaoqi@clipworks.com>
2021-10-12 07:46:06 +0800

    444

--- TestDemo.py
+++ TestDemo.py
@@ -3,15 +3,85 @@
 日期：2021年10月03日
 """
 '''
-现有字符串：str1="python cainiao 666"
-1.找出第五个字符
-2.请复制一份字符串，保存为str_two（使用赋值）
-3.请找出最中间的字符
+将字符串中单词的位置反转， “hello xiao mi”转化为“mi xiao hello”
 '''
-# 第一题
-str1 = 'python cainiao 666'
-index1 = str1[4]
-print(index1)
-# 第二题
-str_two = str1
-# 第三题
+# s = "hello xiao mi"
+# s_split = s.split(" ")
+# print(s_split)
+# s_split.reverse()
+# print(s_split)
+# s_join = " ".join(s_split)
+# print(s_join)
+'''
+打印100遍hello python，第50遍不打印
+'''
+# index = 1
+# while index < 101:
+#     if index == 50:
+#         index += 1
+#         continue
+#     print("hello python")
+#     print(index)
+#     index += 1
+'''
+打印九九乘法表
+1*1=1
+1*2=1 2*2=4
+1*3=3 2*3=6 3*3=9
+'''
+# for column in range(1, 10):
+#     for row in range(1, column + 1):
+#         print(f"{row}*{column}={row * column}", end=' ')
+#     print()
+'''
+实现把字符串 st_r='woaixuexi'中的任意小写变成大写输出（通过输入函数完成）
+'''
+# st_r = "woaixuexi"
+# value = input("请输入您要转化的字母：")
+# value_upper = value.upper()
+# print(st_r.replace(value, value_upper))
+
+'''
+分别打印10以内的所有偶数和奇数，并存入不同的列表当中
+'''
+# oushu = []
+# jishu = []
+# for value in range(0, 11):
+#     print(value)
+#     if value % 2 == 0:
+#         oushu.append(value)
+#     else:
+#         jishu.append(value)
+# print(oushu, jishu)
+'''
+请写一段代码实现删除一个列表中list=[1,3,6,9,1,8] 重复的元素
+'''
+# li_st = [1, 3, 6, 9, 1, 8]
+# result = list(set(li_st))
+# print(result)
+'''
+把字符串 user_controller转化为驼峰命名UserController
+'''
+# result = []
+# value = 'user_controller'
+# value_split = value.split('_')
+# for vs in value_split:
+#     print(vs)
+#     vs_title = vs.title()
+#     result.append(vs_title)
+# print(result)
+# join = ''.join(result)
+# print(join)
+'''
+python实现斐波那契
+'''
+
+value = int(input("想要几位斐波那契数列："))
+
+li_st = []
+for i in range(value):
+    if i == 0 or i == 1:
+        li_st.append(1)
+    else:
+        li_st.append(li_st[i - 2] + li_st[i - 1])
+print(li_st)
--- list_demo.py
+++ list_demo.py
@@ -4,67 +4,68 @@
 """
 # 定义一个列表
 student = ['小明', '小红', '小刚']
-print(student)
-# 增删改查
-# 查
-print(student[0])
-print(student[-1])
-# 改
-student[0] = '改造小明'
-print(student)
-# 增
-# 末尾追加
-student.append("新增01")
-print(student)
-# 制定位置新增
-student.insert(3, "新增02")
-print(student)
-# 合并列表
-student02 = ["张三"]
-student.extend(student02)
-print(student)
-# 删
-del student[0]
-print(student)
-# 弹出（默认弹出最后一位）
-student_pop = student.pop()
-print(student_pop)
-student_pop02 = student.pop(0)
-print(student_pop02)
-# 根据值删除
-print(student)
-student.remove('小刚')
-print(student)
-# 组织列表
+# print(student)
+# # 增删改查
+# # 查
+# print(student[0])
+# print(student[-1])
+# # 改
+# index = student.index('小明')
+# student[index] = '改造小明'
+# print(student)
+# # 增
+# # 末尾追加
+# student.append("新增01")
+# print(student)
+# # 制定位置新增
+# student.insert(3, "新增02")
+# print(student)
+# # 合并列表
+# student02 = ["张三"]
+# student.extend(student02)
+# print(student)
+# # 删
+# del student[0]
+# print(student)
+# # 弹出（默认弹出最后一位）
+# student_pop = student.pop()
+# print(student_pop)
+# student_pop02 = student.pop(0)
+# print(student_pop02)
+# # 根据值删除
+# print(student)
+# student.remove('小刚')
+# print(student)
+# # 组织列表
 # 按照顺序对列表进行永久排序
 cars = ['bmw', 'toyota', 'audi', 'subaru']
-cars.sort()
-print(cars)
-# 相反顺序对列表进行永久排序
-cars.sort(reverse=True)
-print(cars)
-# 对列表进行临时排序
-print(sorted(cars))
-print(cars)
-# 对列表进行翻转
-cars.reverse()
-print(cars)
-# 确定列表的长度
-print(len(cars))
-# 遍历
+# cars.sort()
+# print(cars)
+# # 相反顺序对列表进行永久排序
+# cars.sort(reverse=True)
+# print(cars)
+# # 对列表进行临时排序
+# print(sorted(cars))
+# print(cars)
+# # 对列表进行翻转
+# cars.reverse()
+# print(cars)
+# # 确定列表的长度
+# print(len(cars))
+# # 遍历
 for car in cars:
     print(car)
-# 对数字列表进行统计计算
-value = list(range(10))
-print(min(value))
-print(max(value))
-print(sum(value))
-# 切片
-print(cars[0:1])
-# 复制列表
-car02 = cars[:]
-print(cars)
-print(car02)
-car02.append("aaa")
-print(cars)
-print(car02)
+# # 对数字列表进行统计计算
+# value = list(range(10))
+# print(min(value))
+# print(max(value))
+# print(sum(value))
+# # 切片
+# print(cars[0:1])
+# # 复制列表
+# car02 = cars[:]
+# print(cars)
+# print(car02)
+# car02.append("aaa")
+# print(cars)
+# print(car02)
